c9370c6d0c83221f011f36740faa0173c051cebd
Cody Markham <cody.markham@phoenix-opsgroup.com>
2021-01-12 15:43:50 -0500

    Had to tweak the uri for the express/mongoose connection to the db, it works now!

---
+++ league-manager-server/schemas/PageContent.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure
+const PageContentSchema = new Schema(
+    {
+        _id: String,
+        title: String,
+        description: String,
+        buttonText: String,
+        buttonType: String,
+        imageRef: Array,
+        page: String,
+        position: String
+    },
+    {collection: "PageContent"}
+);
+
+// export the new Schema so we can modify it using Node.js
+module.exports = mongoose.model("PageContent", PageContentSchema);
